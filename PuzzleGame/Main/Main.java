a2d4612f78502b6dbe982f9e029f65dad1b33a30
Frankyyeah <98197856+Frankyyeah@users.noreply.github.com>
2023-08-24 12:34:09 +0800

    Add files via upload

---
+++ PuzzleGame/Main/Main.java
@@ -0,0 +1,8 @@
+import ui.GameJFrame;
+import ui.LoginJFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameJFrame();
+    }
+}
---
+++ PuzzleGame/Main/ui/RegisterJFrame.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame() {
+        this.setSize(488, 500);
+        this.setVisible(true);
+
+        this.setTitle("Puzzle Game v1.0");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
